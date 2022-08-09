d376f4dc42ab2bcc1e21d3f90a01d9660dd2bc03
Default Jupyter user <matan.grinberg@gmail.com>
2022-08-09 14:45:13 -0700

    fixing running issues

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from scipy import stats
+import tensorflow as tf
+
 ############
 # Functions
 ############
@@ -49,3 +54,46 @@ def discrete_angles(n, m, s, rand = 1234):
     discrete_distr = stats.rv_discrete(name='discrete_distr', values=(xk, pk), seed=rand)
     thetas = angles[discrete_distr.rvs(size=n)]
     return thetas
+
+
+def test_on_integers(x_train, y_train):
+    names = []
+    names.append('discrete_model_th5_ph3')
+#     names.append('discrete_model_th4_ph3')
+    names.append('discrete_model_th3_ph3')
+#     names.append('discrete_model_th2_ph3')
+    names.append('discrete_model_th2_ph2')
+    names.append('discrete_model_th1_ph1')
+    names.append('discrete_model_th0_ph0')
+#     names.append('discrete_model_mth15_mph15')
+
+    l = len(names)
+
+    models = []
+    predictions = []
+    
+    for i in range(l):
+        models.append(tf.keras.models.load_model('3dmodels/' + names[i]))
+        xr = models[i](x_train).numpy().transpose()[0]
+        if xr.shape[0] > 0:
+            loss = np.sum(-y_train * np.log(xr) - (1-y_train) * np.log(1-xr)) / xr.shape[0]
+            print(names[i], 'has loss', loss)
+        else:
+            print("xr shape is", xr.shape)
+            
+    return
+ 
+
+def learn_parameters(model_pf, x_train, y_train, iterations=20, epochs=1, batch_size=100):
+    e, b = epochs, batch_size
+    x_inputs = x_train[:, 0:2]
+    
+    for i in range(iterations):
+        model_pf.fit(x_inputs, y_train, epochs=e, batch_size=b)
+        print(i,"Fitted result: ", model_pf.trainable_weights[:][0][0])
+        x = model_pf(x_inputs).numpy().transpose()[0]
+#         print(np.sum(-y_train * np.log(x) - (1-y_train) * np.log(1-x)) / x.shape[0])
+
+    return model_pf.trainable_weights[:][0][0]
+    
+ 
