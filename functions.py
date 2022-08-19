cc736d7771b38733e3a8b12e8bafdd22569a526c
Default Jupyter user <matan.grinberg@gmail.com>
2022-08-19 11:37:04 -0700

    finally fixed. must redo all models though

--- functions.py
+++ functions.py
@@ -35,10 +35,10 @@ def spherical_data(n, thetas, phis, rand=1234):
     mx2, my2 = mean_gen(thetas, phis), np.zeros(n)
     vx2, vy2 = varx_gen(thetas, phis), vary_gen(thetas, phis)
     
-    x1, y1 = np.transpose(np.array([np.random.normal(mx1, vx1, size=n), 
-                                    np.random.normal(my1, vy1, size=n), thetas, phis])), np.zeros(n)
-    x2, y2 = np.transpose(np.array([np.random.normal(mx2, vx2, size=n), 
-                                    np.random.normal(my2, vy2, size=n), thetas, phis])), np.ones(n)
+    x1, y1 = np.transpose(np.array([np.random.normal(mx1, np.sqrt(vx1), size=n), 
+                                    np.random.normal(my1, np.sqrt(vy1), size=n), thetas, phis])), np.zeros(n)
+    x2, y2 = np.transpose(np.array([np.random.normal(mx2, np.sqrt(vx2), size=n), 
+                                    np.random.normal(my2, np.sqrt(vy2), size=n), thetas, phis])), np.ones(n)
     
     x, y = np.append(x1, x2, axis=0), np.append(y1, y2, axis=0)
 
@@ -97,9 +97,26 @@ def learn_parameters(model_pf, x_train, y_train, iterations=20, epochs=1, batch_
 
     return model_pf.trainable_weights[:][0][0]
 
- 
-def likelihood_ratio(x, theta, phi):
-    background = (1 / (2 * np.pi)) * np.exp(-(1 / 2) * (np.linalg.norm(x) ** 2))
-    return background
-
+def likelihood_ratio(x):
+    x = tf.transpose(x)
+    theta = x[2]
+    phi = x[3]
     
+    m = (1 - tf.math.cos(phi)) / 2
+    v1 = 1 - tf.math.cos(theta) * tf.math.sin(phi) / 2
+    v2 = 1 - tf.math.sin(theta) * tf.math.sin(phi) / 2
+        
+    background = (1 / (2 * np.pi)) * tf.math.exp(-(1/2) * (tf.square(x[0]) + tf.square(x[1])))
+    signal = (1/(2 * np.pi * tf.math.sqrt(v1 * v2))) * tf.math.exp(-(1/2) * ((tf.square(x[0] - m)) / v1 + (tf.square(x[1])/ v2)))
+    ratio = signal / (background + signal)
+    
+    return ratio
+
+
+
+
+
+
+
+
+
