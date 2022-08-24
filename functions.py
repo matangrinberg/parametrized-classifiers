ebb21e1d59ecd53279eb94956bc276d770d02928
Default Jupyter user <matan.grinberg@gmail.com>
2022-08-24 15:28:53 -0700

    comparing different kernel initializations

--- functions.py
+++ functions.py
@@ -2,6 +2,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy import stats
 import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model, Sequential
+import matplotlib.pyplot as plt
 
 ############
 # Functions
@@ -84,6 +87,21 @@ def test_on_integers(x_train, y_train):
             
     return
  
+def likelihood_ratio(x):
+    x = tf.transpose(x)
+    theta = x[2]
+    phi = x[3]
+    
+    m = (1 - tf.math.cos(phi)) / 2
+    v1 = 1 - tf.math.cos(theta) * tf.math.sin(phi) / 2
+    v2 = 1 - tf.math.sin(theta) * tf.math.sin(phi) / 2
+        
+    background = (1 / (2 * np.pi)) * tf.math.exp(-(1/2) * (tf.square(x[0]) + tf.square(x[1])))
+    signal = (1/(2 * np.pi * tf.math.sqrt(v1 * v2))) * tf.math.exp(-(1/2) * ((tf.square(x[0] - m)) / v1 + (tf.square(x[1])/ v2)))
+    ratio = signal / (background + signal)
+    
+    return ratio
+    
 
 def learn_parameters(model_pf, x_train, y_train, iterations=20, epochs=1, batch_size=100):
     e, b = epochs, batch_size
@@ -102,36 +120,94 @@ def compare_learning(model_pf1, model_pf2, angles, x_train, y_train, iterations=
     e, b = epochs, batch_size
     x_inputs = x_train[:, 0:2]
     print('Initialized at ', model_pf1.trainable_weights[:][0][0], model_pf2.trainable_weights[:][0][0])
-    
+    list1, list2 = [], []
     for i in range(iterations):
         model_pf1.fit(x_inputs, y_train, epochs=e, batch_size=b)
         model_pf2.fit(x_inputs, y_train, epochs=e, batch_size=b)
+        list1.append(model_pf1.trainable_weights[:][0][0])
+        list2.append(model_pf2.trainable_weights[:][0][0])
         print(i,"Fitted result 1: ", model_pf1.trainable_weights[:][0][0])
         print(i,"Fitted result 2: ", model_pf2.trainable_weights[:][0][0])
         
-        angle_errors1 = (angles - model_pf1.trainable_weights[:][0][0]) / angles
-        angle_errors2 = (angles - model_pf2.trainable_weights[:][0][0]) / angles
-        average_error1 = np.sum(np.abs(angle_errors1)) / 2
-        average_error2 = np.sum(np.abs(angle_errors2)) / 2
-        print('After ', i, 'iterations, LE %: ', average_error1 * 100, ' IE %: ', average_error2 * 100)
+    a1, a2 = np.array(list1), np.array(list2)
+    e1 = 100 * np.sum(np.abs((angles - a1) / angles), axis = 1)/2
+    e2 = 100 * np.sum(np.abs((angles - a2) / angles), axis = 1)/2
 
-    return model_pf1.trainable_weights[:][0][0], model_pf2.trainable_weights[:][0][0]
-
-
-def likelihood_ratio(x):
-    x = tf.transpose(x)
-    theta = x[2]
-    phi = x[3]
-    
-    m = (1 - tf.math.cos(phi)) / 2
-    v1 = 1 - tf.math.cos(theta) * tf.math.sin(phi) / 2
-    v2 = 1 - tf.math.sin(theta) * tf.math.sin(phi) / 2
+    plt.plot(np.arange(e1.shape[0]), e1, e2)
         
-    background = (1 / (2 * np.pi)) * tf.math.exp(-(1/2) * (tf.square(x[0]) + tf.square(x[1])))
-    signal = (1/(2 * np.pi * tf.math.sqrt(v1 * v2))) * tf.math.exp(-(1/2) * ((tf.square(x[0] - m)) / v1 + (tf.square(x[1])/ v2)))
-    ratio = signal / (background + signal)
-    
-    return ratio
+
+    return list1, list2
+
+
+def compare_learning_thorough(angles, x_train, y_train, iterations=20, epochs=1, batch_size=100, runs=5, seed=12345):
+    listlist1, listlist2 = [], []
+    for j in range(runs):
+        # KERNEL INITIALIZER
+        s = tf.convert_to_tensor([1.]).shape
+        res = tf.random.uniform(s, maxval=2*np.pi, seed=seed), tf.random.uniform(s, maxval=np.pi, seed=seed)
+        def my_init(shape, dtype=None):
+            return tf.transpose(tf.convert_to_tensor(res))
+
+
+        # LIKELIHOOD MODEL
+        loss_fn0 = tf.keras.losses.BinaryCrossentropy()
+        # Building model_angles which is used to train (theta, phi)
+        inputs_hold0 = tf.keras.Input(shape=(1,))
+        simple_linear0 = Dense(2, use_bias = False, kernel_initializer=my_init)(inputs_hold0)
+        model_angles0 = Model(inputs = inputs_hold0, outputs = simple_linear0)
+        # Building model_parmafinder, inputs, which takes the (x, y) and finds the best (theta, phi)
+        raw_inputs0 = tf.keras.Input(shape=(2,))
+        inputs0 = tf.keras.layers.concatenate([raw_inputs0, model_angles0(tf.ones_like(raw_inputs0)[:,0:1])])
+        output0 = likelihood_ratio(inputs0)
+        model_paramfinder = Model(inputs = raw_inputs0, outputs = output0)
+        model_paramfinder.compile(loss=loss_fn0, optimizer='Adam')
+
+
+        # INTERPOLATED MODEL
+        model_interpolate = tf.keras.models.load_model('3dmodels/discrete_model_mth50_mph50')
+        for l in model_interpolate.layers:
+            l.trainable=False
+        loss_fn = tf.keras.losses.BinaryCrossentropy()
+        # Building model_angles which is used to train (theta, phi)
+        inputs_hold = tf.keras.Input(shape=(1,))
+        simple_linear = Dense(2, use_bias = False, kernel_initializer=my_init)(inputs_hold)
+        model_angles = Model(inputs = inputs_hold, outputs = simple_linear)
+        # Building model_parmafinder, inputs, which takes the (x, y) and finds the best (theta, phi)
+        raw_inputs = tf.keras.Input(shape=(2,))
+        inputs = tf.keras.layers.concatenate([raw_inputs, model_angles(tf.ones_like(raw_inputs)[:,0:1])])
+        output = model_interpolate(inputs)
+        model_interpolate_paramfinder = Model(inputs = raw_inputs, outputs = output)
+        model_interpolate_paramfinder.compile(loss=loss_fn, optimizer='Adam')
+
+
+        model_pf1, model_pf2 = model_paramfinder, model_interpolate_paramfinder
+        e, b = epochs, batch_size
+        x_inputs = x_train[:, 0:2]
+        print('Initialized at ', model_pf1.trainable_weights[:][0][0], model_pf2.trainable_weights[:][0][0])
+        list1, list2 = [], []
+        for i in range(iterations):
+            model_pf1.fit(x_inputs, y_train, epochs=e, batch_size=b)
+            model_pf2.fit(x_inputs, y_train, epochs=e, batch_size=b)
+            list1.append(model_pf1.trainable_weights[:][0][0])
+            list2.append(model_pf2.trainable_weights[:][0][0])
+            print(i,"Fitted result 1: ", model_pf1.trainable_weights[:][0][0])
+            print(i,"Fitted result 2: ", model_pf2.trainable_weights[:][0][0])
+        
+        a1, a2 = np.array(list1), np.array(list2)
+        e1 = 100 * np.sum(np.abs((angles - a1) / angles), axis = 1)/2
+        e2 = 100 * np.sum(np.abs((angles - a2) / angles), axis = 1)/2
+        plt.plot(e1, label='Likelihood, Run # %.0f' % (j+1))
+        plt.plot(e2, label='Interpolated, Run # %.0f' % (j+1))
+        listlist1.append(list1)
+        listlist2.append(list2)
+        
+    plt.legend()
+    plt.xlabel('Iteration')
+    plt.ylabel('Error %')
+        
+
+    return list1, list2
+
 
 
 
